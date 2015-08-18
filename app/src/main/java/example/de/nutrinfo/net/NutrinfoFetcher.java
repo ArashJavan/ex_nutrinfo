8586a5dbcc70d0a1c5c767f7cc676c24873013bc
Arash Javanmard <a.javan.gh@gmail.com>
2015-08-18 23:22:24 +0200

    layout changed

--- app/src/main/java/example/de/nutrinfo/net/NutrinfoFetcher.java
+++ app/src/main/java/example/de/nutrinfo/net/NutrinfoFetcher.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
